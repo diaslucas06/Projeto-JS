ef6f1ff5c92388dcb020392b7be870d8bbd38f42
Raíssa Santos <raissa.medeiros@escolar.ifrn.edu.br>
2026-01-21 22:10:23 -0300

    feat: adicionar funcionalidade de cadastro e exibição de livros

--- conteúdo/app.js
+++ conteúdo/app.js
@@ -1,35 +1,47 @@
-let FormPreencher = document.getElementById('#form-preencher');
-let FormCadastro = document.getElementById('#form-cadastro');
+let FormPreencher = document.getElementById('form-preencher');
+const BotaoAdicionar = document.getElementById('btn');
+const Listaul = document.getElementById('lista');
+let FormCadastro = document.getElementById('form-cadastro');
+let LivrosCadastrados = [];
+let TotalGeral = 0;
+let QuantidadeTotal = 0;
+let MediaTotal = TotalGeral / QuantidadeTotal;
 
-function Number(valor){
-    if (valor == "string") {
-        valor = Number(valor);
-        valor.replace(",",".");
+function atualizar_lista() {
+    Listaul.innerHTML = ''; 
+
+    for (let i = 0; i < LivrosCadastrados.length; i++) {
+        let li = document.createElement('li');
+        let livro = LivrosCadastrados[i];
+
+        li.textContent = `${livro.titulo}\n${livro.genero}\n${livro.descricao}\nQuantidade: ${livro.quantidade}\nValor: R$${livro.valorUnitario}`;
+        Listaul.appendChild(li);
     }
-    return valor.toFixed(2);
 }
 
-let Botao = document.getElementById('btn');
-let Lista = []
-
-Botao.addEventListener('click', function(event) {
+BotaoAdicionar.addEventListener('click', function(event) {
     event.preventDefault();
 
     let titulo = document.getElementsByName('titulo')[0].value;
     let descricao = document.getElementsByName('descricao')[0].value;
-    let Categoria = document.getElementsByName('categoria')[0].value;
-    let Quantidade = document.getElementsByName('quantidade')[0].value;
-    let ValorUnitario = document.getElementsByName('valorUnitario')[0].value;
+    let genero = document.getElementsByName('genero')[0].value;
+    let quantidade = document.getElementsByName('quantidade')[0].value;
+    let valorUnitario = document.getElementsByName('valorUnitario')[0].value;
 
-    Quantidade = Number(Quantidade);
-    ValorUnitario = Number(ValorUnitario);
+    quantidade = Number(quantidade);
+    valorUnitario = Number(valorUnitario.replace(",", ".")).toFixed(2);
 
-    livro = {}
-    livro.titulo = titulo;
-    livro.descricao = descricao;
-    livro.categoria = Categoria;
-    livro.quantidade = Quantidade;
-    livro.valorUnitario = ValorUnitario;
-    
-    Lista.push(livro);
+    TotalGeral += quantidade * valorUnitario;
+    QuantidadeTotal += quantidade;
+
+    const livro = {
+        titulo: titulo,
+        descricao: descricao,
+        genero: genero,
+        quantidade: quantidade,
+        valorUnitario: valorUnitario
+    }
+
+    LivrosCadastrados.push(livro);
+    atualizar_lista();
 });
